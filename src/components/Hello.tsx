c3a4c6832238fe1ee875943bb369fced3d5ffbf4
deshmukh-somesh <deshmukhsomesh7777@gmail.com>
2025-05-20 17:10:10 +0530

    added jsx differneces

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -18,6 +18,7 @@ export default Hello;
 
 // 2.Without using JSX : 
 
+/*
 import React from 'react';
 
 const Hello = () => {
@@ -28,3 +29,49 @@ export default Hello;
 
 // Output : Hello Somesh Deshmukh!
 
+*/
+/*
+React.createElement Parameters Explained
+React.createElement takes three main parameters:
+
+Type: The first parameter specifies the element type
+
+Can be a string for HTML elements (like 'div', 'span', 'button')
+Can be a React component (class or function)
+Can be a React Fragment (React.Fragment)
+
+
+Props: The second parameter is an object containing the properties/attributes
+
+Contains all attributes/props you want to pass to the element
+Can include event handlers like onClick, onSubmit
+Can be null or empty object ({}) if no props are needed
+Includes special props like key, ref, className, style, etc.
+
+
+Children: The third and subsequent parameters are the children
+
+Can be strings for text content
+Can be other React elements (more createElement calls)
+Can be arrays of elements
+Can be null or undefined (no children)
+
+*/
+
+import React from 'react';
+const Hello = () => {
+    return React.createElement("div", { id: "hello", className: "dummyClass" }, React.createElement("h1", null, "Hello Vishwas"))
+}
+
+export default Hello;
+
+
+// JSX differences : 
+
+/*
+Class -> ClassName
+for -> htmlFor
+camelCase property naming convention 
+onclick -> onClick
+tabindex -> tabIndex  
+*/
